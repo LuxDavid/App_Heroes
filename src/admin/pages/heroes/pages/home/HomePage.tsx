9b5118199f8091d8f717f3a198522891309b667a
Diaz Tabares Brayan David <brayandiaz162@gmail.com>
2026-02-07 16:22:52 -0600

    implementacion de query params en la pagina home

--- src/admin/pages/heroes/pages/home/HomePage.tsx
+++ src/admin/pages/heroes/pages/home/HomePage.tsx
@@ -1,19 +1,29 @@
-"use client"
-import { useState } from "react"
-// import { Search} from "lucide-react"
-import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
 import { HeroStats } from "../../components/HeroStats";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
-import { HeroGrid } from "../../components/HeroGrid"
-import { CustomPagination } from "@/components/custom/CustomPagination"
-import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs"
-import { getHeroesByPageAction } from "../../actions/get-heroes-by-page.action"
-import { useQuery } from "@tanstack/react-query"
+import { HeroGrid } from "../../components/HeroGrid";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { getHeroesByPageAction } from "../../actions/get-heroes-by-page.action";
+import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router";
+import { useMemo } from "react";
 
 export const HomePage = () => {
-  const [activeTab, setActiveTab] = useState<
-    'all' | 'favorites' | 'heroes' | 'villains'
-  >('all');
+
+  //Metodo para obtener query params
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // const [activeTab, setActiveTab] = useState<
+  //   'all' | 'favorites' | 'heroes' | 'villains'
+  // >('all');
+
+  const activeTab= searchParams.get('tab') ?? 'all';
+
+  const selectedTab= useMemo(() => {
+    const validTabs= [ 'all', 'favorites' , 'heroes' , 'villains'];
+    return validTabs.includes(activeTab) ? activeTab : 'all';
+  }, [activeTab]);
 
   const {data: heroesResponse}= useQuery({
     queryKey:['heroes'],
@@ -39,25 +49,39 @@ export const HomePage = () => {
         <HeroStats />
 
          {/* Tabs */}
-        <Tabs value={activeTab} className="mb-8">
+        <Tabs value={selectedTab} className="mb-8">
 
           <TabsList className="grid w-full grid-cols-4">
-            <TabsTrigger value="all" onClick={() => setActiveTab('all')}>
+            <TabsTrigger value="all" onClick={() => setSearchParams((prev) => {
+              prev.set('tab', 'all');
+              return prev
+            })}>
               All Characters (16)
             </TabsTrigger>
+
             <TabsTrigger
               value="favorites"
               className="flex items-center gap-2"
-              onClick={() => setActiveTab('favorites')}
+              onClick={() => setSearchParams((prev) => {
+              prev.set('tab', 'favorites');
+              return prev
+            })}
             >
               Favorites (3)
             </TabsTrigger>
-            <TabsTrigger value="heroes" onClick={() => setActiveTab('heroes')}>
+
+            <TabsTrigger value="heroes" onClick={() => setSearchParams((prev) => {
+              prev.set('tab', 'heroes');
+              return prev
+            })}>
               Heroes (12)
             </TabsTrigger>
             <TabsTrigger
               value="villains"
-              onClick={() => setActiveTab('villains')}
+              onClick={() => setSearchParams((prev) => {
+              prev.set('tab', 'villains');
+              return prev
+            })}
             >
               Villains (2)
             </TabsTrigger>
